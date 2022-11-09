22b9f898e1a587cc96fdcd818da4a24795be783f
Zachary Morgan <zsmorgan21@gmail.com>
2022-11-09 09:51:17 -0500

    Initial commit

---
+++ apps/my-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-builders-custom-webpack-sever-repro-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'my-app';
+}
